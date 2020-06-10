a925ecfcb88d570943aaa0d58aecc47f7c076d74
jackie-k <mien803@gmail.com>
2020-06-10 16:48:11 +0900

    06.10.2020 edit

--- myImage.py
+++ myImage.py
@@ -7,9 +7,9 @@ import numpy as np
 import torchvision
 from torchvision import transforms
 
-trans = transforms.Compose([transforms.Resize((100,100)),
+trans = transforms.Compose([transforms.Resize((100, 100)),
                             transforms.ToTensor(),
-                            transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                             ])
 trainset = torchvision.datasets.ImageFolder(root="/Users/data-16/Desktop/clothes",
                                             transform=trans)
@@ -19,8 +19,31 @@ trainset.__getitem__(3)
 plt.show()
 
 len(trainset)
+print(len(trainset))
 
 classes = trainset.classes
 print(classes)
 
+trainloader = DataLoader(trainset,
+                         batch_size=16,
+                         shuffle=False,
+                         num_workers=4)
 
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(labels)
+
+
+def imshow(img):
+    img = img / 2 + 0.5
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1, 2, 0)))
+
+    print(np_img.shape)
+    print((np.transpose(np_img, (1, 2, 0))).shape)
+
+    print(images.shape)
+    imshow(torchvision.utils.make_grid(images, nrow=4))
+    print(images.shape)
+    print((torchvision.utils.make_grid(images)).shape)
+    print("".join("%5s " % classes[labels[j]] for j in range(16)))
